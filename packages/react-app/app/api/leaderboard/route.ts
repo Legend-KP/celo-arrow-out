b65f3cd646a9dc8c79619d1e5fc45ca3b2f334e8
Kushal Paliwal <kushal5paliwal@gmail.com>
2026-05-15 15:48:15 +0530

    143

--- packages/react-app/app/api/leaderboard/route.ts
+++ packages/react-app/app/api/leaderboard/route.ts
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server"
 import {
     submitChallengeScore,
     getChallengeLeaderboard
-} from "@/lib/leaderboard"
+} from "@/lib/Leaderboard"
 
 
 export async function POST(
